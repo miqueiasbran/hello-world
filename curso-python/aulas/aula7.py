2be7cdf948c3a07b54ef17a34ae609f9627bb0c5
Miquéias B <miqueiasbran@gmail.com>
2026-01-19 17:55:50 -0300

    19-01-2026

---
+++ curso-python/aulas/aula7.py
@@ -0,0 +1,33 @@
+'''
+PEP-8: The style Guide for python guide
+
+'''
+
+# Variáveis são usadas para salvar uma informação na memória do computador;
+# PEP8: Inicie variáveis com letras minúsculas, pode usar: números e underline _;
+# O sinal de "=" é o operador de atribuição. Ele é usado para atribuir um valor a uma variável;
+# Uso: nome_variavel = expressão
+
+nome_completo = 'Miquéias B.'
+idade = 17
+maior_de_idade = idade >= 18
+print(f'Olá, {nome_completo}, você possui {idade} anos. É maior de idade? {maior_de_idade}')
+
+str_um = str('1')
+int_um = int('1')
+float_um = float('1.1')
+bool_um = bool('1') 
+
+print(type(bool_um))
+
+'''
+OPERADOORES DE ATRIBUIÇÃO
+
+== igual a
+< menor que
+> maior que
+<= menor ou igual 
+>= maior ou igual a 
+!= diferente de
+
+'''
---
+++ curso-python/aulas/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Miquéias'
+sobrenome = 'B.'
+idade = 26
+ano_de_nascimento = (2026 - idade)
+maior_de_idade = idade >= 18
+altura_metros = 1.69
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_de_nascimento)
+print('É maior de idade? ', maior_de_idade)
+print('Altura em metros: ', altura_metros)

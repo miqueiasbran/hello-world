2308b6e96f8c2c5220aad8fd8cea8e6592b39459
Miquéias B <miqueiasbran@gmail.com>
2026-01-20 15:21:56 -0300

    20-01-2026

--- curso-python/aulas/aula14.py
+++ curso-python/aulas/aula14.py
@@ -1,6 +1,16 @@
-a = 'A'
-b = 'B'
-c = 1.1
-formato = ''
+a = 'AAAAA'
+b = 'BBBBB'
+c = 3.33333333333
+string = 'a={nome1} b={nome2} c={nome3:.1f}'
+formato = string.format(nome1=a, nome2=b, nome3=c)
 
 print(formato)
+
+# Indice: Tudo que tem índice na progamação começa do "0"
+
+'''
+
+ABC
+012 <-- ÍNDICE
+
+'''

bf3e1c7fa2384e082296a6e5973b68ee3d0bdaea
Miquéias B <miqueiasbran@gmail.com>
2026-01-24 23:14:16 -0300

    24-01-2026

---
+++ curso-python/aulas/aula20.py
@@ -0,0 +1,11 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(f'O primeiro valor é maior que o segundo valor: {primeiro_valor}.')
+elif segundo_valor >= primeiro_valor:
+    print(f'O segundo valor é maior que o primeiro valor: {segundo_valor}')
+elif primeiro_valor <= segundo_valor:
+    print(f'O primeiro valor é menor que o segundo valor: {primeiro_valor}')
+elif segundo_valor <= primeiro_valor:
+    print(f'O segundo valor é menor ')
